cd20cd47aacbc196dc0df8ca5cee5c5ddc90b756
tulasiram <shri.tulasiminerals@gmail.com>
2020-11-17 12:52:40 +0530

    Tflite Integer Quantization model added

--- scripts/onnx_to_tflite.py
+++ scripts/onnx_to_tflite.py
@@ -4,11 +4,17 @@ Authors
 """
 
 import onnx
-
+import os
+import cv2
+import imgproc
+import torch
 
 import tensorflow as tf
 from onnx_tf.backend import prepare
+from torch.autograd import Variable
 
+
+dataset_path = '/home/ram/Projects/OCR/test_files/'
 
 # Export model to tensorflow
 onnx_model = onnx.load('../models/craft.onnx')
@@ -27,8 +33,30 @@ converter = tf.lite.TFLiteConverter.from_concrete_functions([concrete_func])
 
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
 
+# Uncomment this line for float16 quantization.
+#converter.target_spec.supported_types = [tf.float16]
+
+# For Full Integer Quantization
+def representative_data_gen():
+    for file in os.listdir(dataset_path)[:10]:
+        file_path = dataset_path+file
+        image = imgproc.loadImage(file_path)
+        image = cv2.resize(image, dsize=(800, 1280), interpolation=cv2.INTER_LINEAR)
+        img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, 1280, interpolation=cv2.INTER_LINEAR, mag_ratio=1.5)
+        ratio_h = ratio_w = 1 / target_ratio
+
+        # preprocessing
+        x = imgproc.normalizeMeanVariance(img_resized)
+        x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
+        x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
+        x = x.cpu().detach().numpy()
+        yield [x]
+
+converter.representative_dataset = representative_data_gen
+
+
 tf_lite_model = converter.convert()
 
-open('craft.tflite', 'wb').write(tf_lite_model)
+open('craft_int.tflite', 'wb').write(tf_lite_model)
 
 print("Converted to tensorflow lite succesfully.")
